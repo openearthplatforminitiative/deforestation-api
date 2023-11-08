245b18a38783472769c3a05a3b0683afed600c8a
A-Stangeland <aleksander.stangeland@gmail.com>
2023-11-08 11:24:01 +0100

    Add health check

---
+++ deforestation_api/routers/healthcheck.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from fastapi import Response
+from healthcheck import HealthCheck
+from deforestation_api.settings import settings
+
+router = APIRouter(tags=["health"])
+
+
+@router.get("/health")
+def liveness() -> dict[str, str]:
+    return {"message": "Ok"}
+
+
+@router.get("/ready", tags=["health"])
+async def ready() -> Response:
+    health = HealthCheck()
+
+    health.add_section("version", settings.version)
+
+    message, status_code, headers = health.run()
+    return Response(content=message, headers=headers, status_code=status_code)
