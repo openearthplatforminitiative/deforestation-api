6d7e8c1d9cdd06493bca7b90407124fd638427ac
A-Stangeland <aleksander.stangeland@gmail.com>
2023-11-09 15:26:56 +0100

    Refactor models

---
+++ deforestation_api/models/basin.py
@@ -0,0 +1,71 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+from deforestation_api.models.geometry import Polygon
+
+
+class LossYear(BaseModel):
+    year: int = Field(example=2022, description="Year when the loss was detected.")
+    area: float = Field(
+        example=8.5095,
+        description="Total tree cover loss within the basin polygon, in square kilometers.",
+    )
+    relative_area: float = Field(
+        example=0.0036,
+        description=(
+            "Tree cover loss within the basin polygon "
+            "relative to the total area of the polygon."
+        ),
+    )
+
+
+class BasinProperties(BaseModel):
+    downstream_id: int = Field(
+        example=1071114980,
+        description=(
+            "Id of the next downstream polygon for the current basin polygon. "
+            "The value 0 means that there is no downstream connection."
+        ),
+    )
+    basin_area: float = Field(
+        example=2350.0, description="Area of the basin polygon in square kilometers."
+    )
+    upstream_area: float = Field(
+        example=29444.1,
+        description=(
+            "Total upstream area in square kilometers, "
+            "including the current polygon."
+        ),
+    )
+    start_year: int = Field(example=2020)
+    end_year: int = Field(example=2022)
+    daterange_tot_treeloss: float = Field(
+        example=35.7217,
+        description=(
+            "Total tree cover loss, in square kilometers, "
+            "within the basin polygon over the time period "
+            "from start_year to end_year (inclusive)"
+        ),
+    )
+    daterange_rel_treeloss: float = Field(
+        example=0.0152,
+        description=(
+            "Tree cover loss within the basin polygon "
+            "relative to the total area of the polygon, "
+            "over the time period from start_year to end_year (inclusive). "
+            "Equivalent to `daterange_tot_treeloss / basin_area`."
+        ),
+    )
+    treeloss_per_year: list[LossYear]
+
+
+class DeforestationBasinFeature(BaseModel):
+    id: int = Field(example=1071119930)
+    type: Literal["Feature"]
+    properties: BasinProperties
+    geometry: Polygon
+
+
+class DeforestationBasinGeoJSON(BaseModel):
+    type: Literal["FeatureCollection"]
+    features: list[DeforestationBasinFeature]
---
+++ deforestation_api/models/geometry.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Annotated, Literal
+from pydantic import BaseModel, Field
+
+Longitude = Annotated[float, Field(ge=-180, le=180)]
+Latitude = Annotated[float, Field(ge=-90, le=90)]
+Position = tuple[Longitude, Latitude]
+LinearRing = Annotated[list[Position], Field(min_length=4)]
+PolygonCoords = list[LinearRing]
+
+
+class FeatureCollectionType(Enum):
+    FeatureCollection = "FeatureCollection"
+
+
+class FeatureType(Enum):
+    Feature = "Feature"
+
+
+class Polygon(BaseModel):
+    type: Literal["Polygon"]
+    coordinates: PolygonCoords
