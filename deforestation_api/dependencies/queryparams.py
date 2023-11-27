efd067c27d9e1d52f7f9de4d333456d58a3445f6
A-Stangeland <aleksander.stangeland@gmail.com>
2023-11-27 13:48:31 +0100

    Add bounding box query

--- deforestation_api/dependencies/queryparams.py
+++ deforestation_api/dependencies/queryparams.py
@@ -1,22 +1,48 @@
 from typing import Annotated
 
-from fastapi import Depends, Query
+from fastapi import Depends, Query, HTTPException
 
 
 def coordinates(
     lon: Annotated[
-        float,
-        Query(description="Longitude of the coordinate to retrieve data for"),
-    ],
+        float | None,
+        Query(description="Longitude of the point to retrieve data for"),
+    ] = None,
     lat: Annotated[
-        float,
-        Query(description="Latitude of the coordinate to retrieve data for"),
-    ],
-) -> (float, float):
-    return lon, lat
+        float | None,
+        Query(description="Latitude of the point to retrieve data for"),
+    ] = None,
+    lon_min: Annotated[
+        float | None,
+        Query(description="Minimal longitude for the query bounding box."),
+    ] = None,
+    lat_min: Annotated[
+        float | None,
+        Query(description="Minimal latitude for the query bounding box."),
+    ] = None,
+    lon_max: Annotated[
+        float | None,
+        Query(description="Maximal longitude for the query bounding box."),
+    ] = None,
+    lat_max: Annotated[
+        float | None,
+        Query(description="Maximal latitude for the query bounding box."),
+    ] = None,
+) -> tuple[float, float] | tuple[float, float, float, float]:
+    if lon is not None and lat is not None:
+        return lon, lat
+    bbox = (lon_min, lat_min, lon_max, lat_max)
+    if all(v is not None for v in bbox):
+        return bbox
+    raise HTTPException(
+        status_code=400,
+        detail="Missing coordinates in request. Request should either include both lat and lon, or all of lon_min, lat_min, lon_max, lat_max.",
+    )
 
 
-CoordinatesDep = Annotated[tuple[float, float], Depends(coordinates)]
+CoordinatesDep = Annotated[
+    tuple[float, float] | tuple[float, float, float, float], Depends(coordinates)
+]
 
 
 def date_range(
