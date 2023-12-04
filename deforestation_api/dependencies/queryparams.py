9a24e8c83646134235f5d18c3d8ebcc983542898
A-Stangeland <aleksander.stangeland@gmail.com>
2023-12-04 15:03:08 +0100

    Add checks to coordinates and date range

--- deforestation_api/dependencies/queryparams.py
+++ deforestation_api/dependencies/queryparams.py
@@ -33,14 +33,29 @@ def coordinates(
         Query(description="Maximal latitude of the bounding box to retrieve data for."),
     ] = None,
 ) -> tuple[float, float] | tuple[float, float, float, float]:
-    if lon is not None and lat is not None:
-        return lon, lat
+    point = (lon, lat)
     bbox = (min_lon, min_lat, max_lon, max_lat)
-    if all(v is not None for v in bbox):
+    if None not in point and None not in bbox:
+        raise HTTPException(
+            status_code=400,
+            detail=(
+                "Ambiguous coordinates. Request contains both point coordinates (lon,"
+                " lat) and bounding box coordinates (min_lon, min_lat, max_lon,"
+                " max_lat). Request should only include either point coordinates or"
+                " bounding box coordinates, not both."
+            ),
+        )
+    if None not in point:
+        return point
+    if None not in bbox:
         return bbox
     raise HTTPException(
         status_code=400,
-        detail="Missing coordinates in request. Request must either include both lat and lon, or all of min_lon, min_lat, max_lon, max_lat.",
+        detail=(
+            "Missing coordinates in request. Request must either include point"
+            " coordinates (lon, lat) or bounding box coordinates (min_lon, min_lat,"
+            " max_lon, max_lat)."
+        ),
     )
 
 
@@ -59,6 +74,11 @@ def date_range(
         Query(description="Last year in the data range to return data for."),
     ] = 2022,
 ) -> tuple[int, int]:
+    if start_year > end_year:
+        raise HTTPException(
+            status_code=400,
+            detail="Ivalid date range. start_year can not be greater than end_year.",
+        )
     return start_year, end_year
 
 
